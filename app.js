0848552816f3ef6083c55e805cf03cd4926fbf6a
Vance McGrady <vancemcgrady@gmail.com>
2021-10-17 14:00:26 -0700

    got dynamic greeting working in js. Pre css typwriter effect update.

--- app.js
+++ app.js
@@ -2,20 +2,20 @@ function greet() {
   const todaysDate = new Date();
   const currentTime = todaysDate.getHours();
   const yourName = "Vance";
-  let greeting = document.getElementById("greeting").textContent;
+  let greeting = document.getElementById("greeting");
 
   console.log(todaysDate);
   console.log(currentTime);
   console.log(yourName);
-
-  if (currentTime > 5) {
-    greeting = "You should get some sleep, " + yourName;
+  console.log(greeting);
+  if (currentTime < 5) {
+    greeting.innerHTML = "You should get some sleep, " + yourName;
   } else if (currentTime > 5 && currentTime < 12) {
-    greeting = "Good morning, " + yourName;
+    greeting.innerHTML = "Good morning, " + yourName;
   } else if (currentTime >= 12 && currentTime < 18) {
-    greeting = "Good afternoon, " + yourName;
+    greeting.innerHTML = "Good afternoon, " + yourName;
   } else {
-    greeting = "Good evening, " + yourName;
+    greeting.innerHTML = "Good evening, " + yourName;
   }
 
   console.log(greeting);
