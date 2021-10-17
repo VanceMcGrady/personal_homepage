e88b69c2bb7f909e7d9a77cc2b87dc8cce22b5b5
Vance McGrady <vancemcgrady@gmail.com>
2021-10-17 13:34:26 -0700

    javascript greeting working in console but not rendering properly on page

--- app.js
+++ app.js
@@ -1,4 +1,24 @@
-const todaysDate = new Date();
-const currentTime = todaysDate.getHours();
+function greet() {
+  const todaysDate = new Date();
+  const currentTime = todaysDate.getHours();
+  const yourName = "Vance";
+  let greeting = document.getElementById("greeting").textContent;
 
-let greeting = document.getElementById();
+  console.log(todaysDate);
+  console.log(currentTime);
+  console.log(yourName);
+
+  if (currentTime > 5) {
+    greeting = "You should get some sleep, " + yourName;
+  } else if (currentTime > 5 && currentTime < 12) {
+    greeting = "Good morning, " + yourName;
+  } else if (currentTime >= 12 && currentTime < 18) {
+    greeting = "Good afternoon, " + yourName;
+  } else {
+    greeting = "Good evening, " + yourName;
+  }
+
+  console.log(greeting);
+}
+
+greet();
