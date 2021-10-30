a4d0d83aacd0541dfd2d52b46bcf501f774dd166
Vance McGrady <vancemcgrady@gmail.com>
2021-10-30 11:03:13 -0700

    working fine

--- app.js
+++ app.js
@@ -4,10 +4,6 @@ function greet() {
   const yourName = "Vance.";
   let greeting = document.getElementById("greeting");
 
-  console.log(todaysDate);
-  console.log(currentTime);
-  console.log(yourName);
-  console.log(greeting);
   if (currentTime < 5) {
     greeting.innerHTML = "You should get some sleep, " + yourName;
   } else if (currentTime > 5 && currentTime < 12) {
@@ -17,8 +13,6 @@ function greet() {
   } else {
     greeting.innerHTML = "Good evening, " + yourName;
   }
-
-  console.log(greeting);
 }
 
 greet();
